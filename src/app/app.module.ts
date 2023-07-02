c31253893f7cf3a8c0e222f3bb107595408a1383
Kobi Soffer <kobisoffer@gmail.com>
2023-07-02 18:03:15 +0300

    Added NGRX

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './app-store/reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
 /**
  * APP MODULES
  */
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { FeaturesModule } from './features/features.module';
+import { AppEffects } from './app-store/effects/app.effects';
 
 @NgModule({
   declarations: [AppComponent],
@@ -20,6 +25,11 @@ import { FeaturesModule } from './features/features.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    StoreModule.forRoot(reducers, {
+      metaReducers,
+    }),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
+    EffectsModule.forRoot([AppEffects]),
   ],
   providers: [],
   bootstrap: [AppComponent],
